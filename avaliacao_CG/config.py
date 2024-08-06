fa3225d63ab59a4dd6e7fa679c3b8a990dd31aa8
Thomaznavajas <Thomaznavajas@gmail.com>
2024-08-06 19:42:41 -0300

    Funções de Texturas
    Aplicação já renderiza uma textura com a imagem da escada do C3, porém em proporções erradas

--- avaliacao_CG/config.py
+++ avaliacao_CG/config.py
@@ -2,60 +2,38 @@ import glfw
 import glfw.GLFW as GLFW_CONSTANTS
 from OpenGL.GL import *
 from OpenGL.GL.shaders import compileProgram, compileShader
+from PIL import Image
 import numpy as np
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
-data_type_vertex = np.dtype({
-    'names': ['x', 'y', 'z', 'color'],
-    'formats': [np.float32, np.float32, np.float32, np.uint32],
-    'offsets': [0, 4, 8, 12],
-    'itemsize': 16})
+
+
+def load_image(filepath):
+    """ Carrega uma imagem e retorna como um objeto Image. """
+    return Image.open(filepath).convert('RGBA')
+
+
+def create_texture(image):
+    """ Cria uma textura OpenGL a partir de um objeto Image. """
+    texture = glGenTextures(1)
+    glBindTexture(GL_TEXTURE_2D, texture)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image.tobytes())
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    glBindTexture(GL_TEXTURE_2D, 0)
+    return texture
 
 
 def create_shader_program(vertex_filepath: str, fragment_filepath: str) -> int:
-    """
-        Compile and link a shader program.
+    with open(vertex_filepath, 'r') as file:
+        vertex_src = file.read()
+    with open(fragment_filepath, 'r') as file:
+        fragment_src = file.read()
 
-        Parameters:
-
-            vertex_filepath: filepath to the vertex module source code.
-
-            fragment_filepath: filepath to the fragment module source code.
-
-        returns:
-
-            A handle to the created shader program.
-    """
-
-    vertex_module = create_shader_module(vertex_filepath, GL_VERTEX_SHADER)
-    fragment_module = create_shader_module(fragment_filepath, GL_FRAGMENT_SHADER)
-
-    shader = compileProgram(vertex_module, fragment_module)
-
-    glDeleteShader(vertex_module)
-    glDeleteShader(fragment_module)
-
+    vertex_shader = compileShader(vertex_src, GL_VERTEX_SHADER)
+    fragment_shader = compileShader(fragment_src, GL_FRAGMENT_SHADER)
+    shader = compileProgram(vertex_shader, fragment_shader)
+    glDeleteShader(vertex_shader)
+    glDeleteShader(fragment_shader)
     return shader
-
-
-def create_shader_module(filepath: str, module_type: int) -> int:
-    """
-        Compile a shader module.
-
-        Parameters:
-
-            filepath: filepath to the module source code.
-
-            module_type: indicates which type of module to compile.
-
-        returns:
-
-            A handle to the created shader module.
-    """
-
-    source_code = ""
-    with open(filepath, "r") as file:
-        source_code = file.readlines()
-
-    return compileShader(source_code, module_type)
